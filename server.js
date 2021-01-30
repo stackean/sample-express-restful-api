0da95e404f0b2e538c56218423857f9dd24f2a09
Emre Savaş <e@emresavas.com>
2021-01-30 18:46:40 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const app = express();
+
+let users = {
+    1: {
+        id: '1',
+        username: 'Jane Doe',
+    },
+    2: {
+        id: '2',
+        username: 'Joe Doe',
+    },
+};
+
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'Goodbye World',
+        userId: '2',
+    },
+};
+
+app.get('/', (req, res) => {
+    return res.send(Object.values(users));
+});
+
+app.get('/users', (req, res) => {
+    return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId]);
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000')
+})
