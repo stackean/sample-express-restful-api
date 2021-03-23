de422e843bd543b3428fb70e13497254f778b0c9
Emre Savaş <e@emresavas.com>
2021-03-23 20:37:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.get('/messages/:messageId', (req, res) => {
 });
 
 app.listen(process.env.SERVER_PORT, function () {
-  console.log('Listening on port 3000')
+  console.log('Listening on port ' + process.env.SERVER_PORT)
 })
